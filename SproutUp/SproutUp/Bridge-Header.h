1da73cac8e4f2d550bef2c60900f6dc44390c86a
Apurv Sethi <Apurv@Apurvs-MacBook-Pro.local>
2015-06-04 12:25:52 -0400

    Added FB login (pending adding SproutUp app on FB, plist update)

---
+++ SproutUp/SproutUp/Bridge-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridge-Header.h
+//  SproutUp
+//
+//  Created by Apurv Sethi on 6/3/15.
+//  Copyright (c) 2015 SproutUp Co. All rights reserved.
+//
+
+#ifndef SproutUp_Bridge_Header_h
+#define SproutUp_Bridge_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
---
+++ SproutUp/SproutUp/FBLoginViewController.swift
@@ -0,0 +1,60 @@
+//
+//  FBLoginViewController.swift
+//  SproutUp
+//
+//  Created by Apurv Sethi on 6/4/15.
+//  Copyright (c) 2015 SproutUp Co. All rights reserved.
+//
+
+import UIKit
+
+class FBLoginViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            // User is already logged in, do work such as go to next view controller.
+        }
+        else
+        {
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = CGPointMake(self.view.center.x, self.view.bounds.height*3/4)
+            loginView.readPermissions = ["public_profile", "email", "user_friends"]
+            loginView.delegate = self
+        }
+        
+    }
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        println("User Logged In")
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                // Do work
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        println("User Logged Out")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
